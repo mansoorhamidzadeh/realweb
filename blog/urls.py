15d89026e7a4e4b5ac972c14b794ca1c2ed956bd
mansoorhamidzadeh <mansoor.hamidzadeh@gmail.com>
2019-05-24 16:03:48 +0430

    6

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home,about
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView,
+                    )
+from .views import home, about
 
 urlpatterns = [
 
-    path('', home, name='blog-home'),
-    path('about/',about,name='blog-about')
+    path('', PostListView.as_view(), name='blog-home'),
+    path('about/', about, name='blog-about'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete')
 ]
